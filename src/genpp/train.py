306a66c4fa314a82e0322aa2cfc2f308baf1aafb
Moritz <moritz.feik@gmail.com>
2025-10-16 19:06:29 +0200

    🧹Cleanup

--- src/genpp/train.py
+++ src/genpp/train.py
@@ -22,19 +22,17 @@ def train(cfg: DictConfig) -> None:
     try:
         datamodule.prepare_data()
 
-        model = hydra.utils.instantiate(
+        model: L.LightningModule = hydra.utils.instantiate(
             cfg.model, rescaler=datamodule.y_reverseModules if cfg.model.use_rescaler else None
         )
         model.compile()
-        logger = None
+        logger = False
         if hasattr(cfg, "logger") and cfg.logger:
             logger = hydra.utils.instantiate(cfg.logger)
             logger.log_hyperparams(OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True))
 
         # Skip sanity checks since the callback needs to be fitted first and this would fail otherwise
-        trainer = hydra.utils.instantiate(
-            cfg.trainer, logger=logger, detect_anomaly=False, num_sanity_val_steps=0
-        )
+        trainer: L.Trainer = hydra.utils.instantiate(cfg.trainer, logger=logger)
 
         trainer.fit(model, datamodule)
     finally:
